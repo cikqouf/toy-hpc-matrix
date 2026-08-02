5dd8b9dd48df9438e0cb7d40404359eb9bf163d4
cikqouf <3831269l@gmail.com>
2026-08-02 17:02:30 +0800

    添加项目文件。

---
+++ src/main.cpp
@@ -0,0 +1,37 @@
+﻿#include <benchmark/benchmark.h>
+#include <random>
+
+#define MAT_THREAD_NUMS 16
+#include "mat.hpp"
+
+static void BM_MatMul(benchmark::State& state) {
+    using T = double;
+
+    int p = state.range(0);
+    int q = state.range(1);
+    int s = state.range(2);
+
+    std::normal_distribution<T> dist(3.0, 1.0);
+
+    mat<T> A(p, q, dist);
+    mat<T> B(q, s, dist);
+
+    for (auto _ : state) {
+        mat<T> C = A * B;
+        benchmark::DoNotOptimize(C.pData);
+    }
+
+    double flops = 2.0 * p * q * s;
+    state.counters["GFLOPS"] = benchmark::Counter(flops / 1e9, benchmark::Counter::kIsRate);
+}
+
+// ----------------------------
+// 参数（多个 p/q/s）
+// ----------------------------
+BENCHMARK(BM_MatMul)
+->Args({ 512, 512, 512 })
+->Args({ 1024, 1024, 1024 })
+->Args({ 2048, 2048, 2048 })
+->Args({ 2312, 3431, 4343 });
+
+BENCHMARK_MAIN();
---
+++ unitTestCorrectness/unitTestCorrectness.cpp
@@ -0,0 +1,52 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+#include "mat.hpp"
+#include <random>
+#include <cmath>
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace unitTestCorrectness
+{
+    TEST_CLASS(unitTestCorrectness)
+    {
+    public:
+
+        void CheckMatMul(int p, int q, int s)
+        {
+            using T = float;
+
+            std::mt19937 rng(123);
+            std::normal_distribution<T> dist(3.0, 1.0);
+
+            mat<T> A(p, q, dist);
+            mat<T> B(q, s, dist);
+
+            mat<T> C = A * B;
+            mat<T> C2(p, s, 0.0);
+
+            // naive reference
+            for (int i = 0; i < p; ++i)
+                for (int k = 0; k < q; ++k)
+                    for (int j = 0; j < s; ++j)
+                        C2.pData[i * s + j] +=
+                        A.pData[i * q + k] * B.pData[k * s + j];
+
+            // correctness check
+            for (int i = 0; i < C.len; ++i)
+            {
+                float diff = std::abs(C.pData[i] - C2.pData[i]);
+                Assert::IsTrue(diff < 1e-2f, L"Matrix multiplication mismatch");
+            }
+        }
+
+        TEST_METHOD(Test_MatMul_0_to_1000)
+        {
+            for (int n = 0; n <= 1000; ++n)
+            {
+                CheckMatMul(n, n, n);
+            }
+        }
+    };
+}
